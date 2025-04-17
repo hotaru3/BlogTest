86d54dc47b3fbe28cac778f14c2e18884359c207
hotarufirefly3 <popopopopopo33@gmail.com>
2025-04-17 21:44:04 +0900

    microCMSのコンテンツを表示

--- microcms-blog/nuxt.config.ts
+++ microcms-blog/nuxt.config.ts
@@ -2,8 +2,10 @@
 export default defineNuxtConfig({
   modules: ["nuxt-microcms-module"],
   microCMS: {
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: process.env.MICROCMS_API_KEY,
+    serviceDomain: "blogkadai",
+    apiKey: "HgmmZhMj70FwElDSU8HrqrVzFDupMpiAQWUh",
+    //serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+    //apiKey: process.env.MICROCMS_API_KEY,
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
--- microcms-blog/pages/index.vue
+++ microcms-blog/pages/index.vue
@@ -1,4 +1,41 @@
-<template lang="pug">
-.index
-    p 表示テスト
-</template>
+<template>
+    <h1>Nuxt3 Jamstack Blogs</h1>
+    <ul>
+      <li v-for="blog in data?.contents" :key="blog.id">
+        <NuxtLink :to="`/${blog.id}`">
+          <img
+            :src="blog.eyecatch?.url"
+            :width="blog.eyecatch?.width"
+            :height="blog.eyecatch?.height"
+            alt=""
+          />
+          <div>
+            <div>
+              {{ blog.category?.name }}
+            </div>
+            <div>
+              {{ blog.title }}
+            </div>
+            <div>
+              {{ blog.publishedAt ?? blog.createdAt }}
+            </div>
+          </div>
+        </NuxtLink>
+      </li>
+    </ul>
+  </template>
+
+
+<script setup lang="ts">
+import type { MicroCMSImage, MicroCMSListContent } from "microcms-js-sdk";
+export type Blog = {
+  title?: string;
+  content?: string;
+  eyecatch?: MicroCMSImage;
+  category: (MicroCMSListContent & Category) | null;
+};
+const { data } = await useMicroCMSGetList<Blog>({
+  endpoint: "blogs",
+});
+console.log(data)
+</script>
