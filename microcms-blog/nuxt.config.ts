aac2caff9903b76721625e8c50791dbf4e34f4a4
hotarufirefly3 <popopopopopo33@gmail.com>
2025-04-17 21:13:56 +0900

    プラグイン導入

--- microcms-blog/nuxt.config.ts
+++ microcms-blog/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["nuxt-microcms-module"],
+  microCMS: {
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
