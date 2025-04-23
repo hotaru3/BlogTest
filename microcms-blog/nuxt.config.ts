df6d04a78e397945da0fb7a580731bc6b3467396
hotarufirefly3 <popopopopopo33@gmail.com>
2025-04-23 21:55:26 +0900

    envの内容を適用

--- microcms-blog/nuxt.config.ts
+++ microcms-blog/nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const { MICROCMS_SERVICE_DOMAIN, MICROCMS_API_KEY } = process.env;
 import tailwindTypography from "@tailwindcss/typography";
 export default defineNuxtConfig({
   modules: ["nuxt-microcms-module", "@nuxtjs/tailwindcss"],
   microCMS: {
-    serviceDomain: "blogkadai",
-    apiKey: "HgmmZhMj70FwElDSU8HrqrVzFDupMpiAQWUh",
+    serviceDomain: MICROCMS_SERVICE_DOMAIN,
+    apiKey: MICROCMS_API_KEY,
     //serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
     //apiKey: process.env.MICROCMS_API_KEY,
   },
